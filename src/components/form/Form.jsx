1224482abbb53975e9b7eaa330cca7238681024a
MinosFenn <45685449+MinosFenn@users.noreply.github.com>
2021-12-05 14:50:43 +0100

    form reworked

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,33 +1,57 @@
-// Make sure to run npm install @formspree/react
-// For more help visit https://formspr.ee/react-help
 import React from 'react';
-
 import { useForm, ValidationError } from '@formspree/react';
+import './form.css';
 
 function Form() {
   const [state, handleSubmit] = useForm('mdobnqkd');
   if (state.succeeded) {
-    return <p>Thanks for joining!</p>;
+    return (
+      <div className="gpt3__form-heading">
+        <h1 className="gradient__text">Thanks for reaching out to us!</h1>;
+      </div>
+    );
   }
   return (
-    <form method="POST" onSubmit={handleSubmit}>
-      <label htmlFor="email">
-        Email Address
-        <input id="email" type="email" name="email" />
-      </label>
-      <ValidationError prefix="Email" field="email" errors={state.errors} />
-      <textarea id="message" name="message" />
-      <ValidationError prefix="Message" field="message" errors={state.errors} />
-      <div
-        className="g-recaptcha"
-        data-sitekey="6LeWpnwdAAAAADH7xIEYazCcLY1Yyf-sQ9IpqdX-"
-      />
-
-      <button type="submit" disabled={state.submitting}>
-        Submit
-      </button>
-    </form>
+    <div data-aos="fade-left" className="gpt3__form">
+      <div className="gpt3__form-heading">
+        <h1 className="gradient__text">Get in touch with Gefyra</h1>{' '}
+      </div>
+      <form onSubmit={handleSubmit}>
+        {' '}
+        <div className="gpt3__form_element">
+          <input
+            id="email"
+            type="email"
+            name="email"
+            placeholder="youremail@mail.com"
+          />
+          <ValidationError prefix="Email" field="email" errors={state.errors} />{' '}
+        </div>
+        <div className="gpt3__form_element">
+          <textarea
+            rows="5"
+            cols="50"
+            id="message"
+            name="message"
+            placeholder="your message"
+          />
+          <ValidationError
+            prefix="Message"
+            field="message"
+            errors={state.errors}
+          />
+        </div>
+        <div className="gpt3__form_button">
+          <button
+            className="gpt3__form-btn"
+            type="submit"
+            disabled={state.submitting}
+          >
+            Submit
+          </button>
+        </div>
+      </form>
+    </div>
   );
 }
-
 export default Form;
