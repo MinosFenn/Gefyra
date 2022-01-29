7bae3976dfc4b11aac98340696a01c8d43f27582
MinosFenn <45685449+MinosFenn@users.noreply.github.com>
2022-01-29 17:52:41 +0100

    navbar hidden on scroll

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import Aos from 'aos';
+// import Aos from 'aos';
 import 'aos/dist/aos.css';
 import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import logo from '../../assets/logo-svg/Gefyra-04-newblue.svg';
@@ -7,11 +7,43 @@ import './navbar.css';
 
 export default function Navbar() {
   const [toggleMenu, setToggleMenu] = useState(false);
+  const [show, setShow] = useState(true);
+  let lastScrollY = window.scrollY;
+
+  const controlNavbar = () => {
+    if (lastScrollY < window.scrollY) {
+      setShow(true);
+    } else {
+      setShow(false);
+    }
+    lastScrollY = window.scrollY;
+  };
+
   useEffect(() => {
-    Aos.init({ duration: 3000 });
+    window.addEventListener('scroll', controlNavbar);
+    return () => {
+      window.removeEventListener('scroll', controlNavbar);
+    };
   }, []);
+  // const nav = document.querySelector('.gef__navbar');
+  // let lastScrollY = window.scrollY;
+  // window.addEventListener('scroll', () => {
+  //   if (lastScrollY < window.scrollY) {
+  //     nav.className.add('nav--hidden');
+  //   } else {
+  //     nav.className.remove('nav--hidden');
+  //   }
+  //   lastScrollY = window.scrollY;
+  // });
+
+  // useEffect(() => {
+  //   Aos.init({ duration: 3000 });
+  // }, []);
   return (
-    <div data-aos="fade-down" className="gef__navbar">
+    <div
+      // data-aos="fade-down"
+      className={`gef__navbar ${show && 'gef__navbar__style'}`}
+    >
       <div className="gef__navbar-links">
         <div className="gef__navbar-links_logo">
           <img src={logo} />
